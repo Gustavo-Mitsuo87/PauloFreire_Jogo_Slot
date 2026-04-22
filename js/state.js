b54a8642e463121ed221bcd92a259f92bfd0d671
raphamelog <raphaguedesmelo@gmail.com>
2026-04-22 11:03:59 -0300

    Inicio do state.js

---
+++ js/state.js
@@ -0,0 +1,34 @@
+/* -> Controlar dados do jogo 
+saldo do jogador 
+custo por rodada
+atualização do saldo 
+reset do jogo
+*/
+const saldoInicio = 0;
+const saldoAtual = 0;
+const contaRodada = 0;
+
+
+function saldoJogador(valor) {
+    saldoInicio = valor;
+    
+}
+
+function custoPorRodada() {
+
+}
+
+function atualizacaoSaldo() {
+
+}
+
+function rodada() {
+    contaRodada += 1;
+}
+
+function reset() {
+
+}
+
+
+export default {saldoJogador, custoPorRodada, atualizacaoSaldo, reset};
