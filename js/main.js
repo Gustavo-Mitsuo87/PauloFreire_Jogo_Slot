e1e07c598e5b20b5cd6b669204abc82d4111eab8
Gustavo-Mitsuo87 <gustavomitsuo1@gmail.com>
2026-04-29 23:36:36 -0300

    Inicio da integração

---
+++ js/main.js
@@ -0,0 +1,79 @@
+import state from "./state.js";
+
+// Isso aqui é para entrar no slot
+const btnEntrar = document.getElementById("btnEntrar");
+
+if (btnEntrar) {
+    btnEntrar.addEventListener("click", (e) => {
+        const nome = document.querySelector('input[name="nome"]').value;
+        const saldo = Number(document.querySelector('input[name="saldo"]').value);
+
+        if (!nome || saldo < 10) {
+            alert("Preencha corretamente!");
+            return;
+        }
+
+        state.defineNome(nome); 
+        state.saldoJogador(saldo);
+        state.salvarEstado();
+
+        // document.querySelector("form").submit(); // banco de dados
+        window.location.href = "slot.html";
+    });
+}
+
+state.carregarEstado();
+
+const saldo = document.getElementById("saldo");
+
+if (saldo) {
+    saldo.textContent = state.getSaldoAtual();
+}
+
+// temporizador
+const timer = document.getElementById("timer");
+
+if (timer) {
+    let tempo = 10;
+
+    function atualizarTimer() {
+        const minutos = Math.floor(tempo / 60);
+        const segundos = tempo % 60;
+
+        timer.textContent =
+            `${String(minutos).padStart(2, "0")}:${String(segundos).padStart(2, "0")}`;
+    }
+
+    atualizarTimer();
+
+    const intervalo = setInterval(() => {
+        tempo--;
+        atualizarTimer();
+
+        if (tempo <= 0) {
+            clearInterval(intervalo);
+            console.log("tempo acabou");
+            fimDeJogo();
+        }
+    }, 1000);
+}
+
+function fimDeJogo() {
+    const modal = document.getElementById("modalFim");
+
+    if (modal) {
+        modal.style.display = "flex";
+    }
+
+    // bloquear cliques
+    document.body.style.pointerEvents = "none";
+    modal.style.pointerEvents = "all";
+}
+
+const btnVoltar = document.getElementById("btnVoltar");
+
+if (btnVoltar) {
+    btnVoltar.addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
+}
--- js/state.js
+++ js/state.js
@@ -4,8 +4,6 @@ let saldoAtual = 0;
 let contaRodada = 0;
 let name = "";
 
-let lista = [];
-
 function getSaldoAtual() {
     return saldoAtual;
 }
@@ -27,8 +25,7 @@ function salvarEstado() { // isso vai garantir que o usuario tente um f5 e perca
         saldoInicio,
         saldoAtual,
         contaRodada,
-        name,
-        lista
+        name
     };/* 
        JSON.stringify transforma objeto em texto
        setItem salva no navegador
@@ -39,19 +36,16 @@ function salvarEstado() { // isso vai garantir que o usuario tente um f5 e perca
 
 function defineNome(nome) {
     name = nome;
-    salvarEstado();
 }
 
 function saldoJogador(valor) { //aqui vai dizer qual o valor inicial do jogador.
     saldoInicio = valor;
     saldoAtual = valor;
-    salvarEstado();
 }
 
 function atualizacaoSaldo(result, valor, multiplicador) { //vai receber se houve ganho ou perda e o valor da aposta
     if (result == true) { //se for true é porque ganhou
         saldoAtual = saldoAtual + (valor * multiplicador);
-        salvarEstado();
         return saldoAtual;
     } else {
         return saldoAtual;
@@ -78,39 +72,7 @@ function reset() { // quando não houver mais rodadas vai reiniciar os valores
     saldoAtual = 0;
     contaRodada = 0;
     name = "";
-    salvarEstado();
 }
-
-function txt(inicio, fim) {
-    lista.push({ nome: name, inicio, fim });
-    salvarEstado();
-}
-
-
-function gerarTXT() { // aqui é onde o filho chora e a mãe não vê
-    let conteudo = lista
-        .map(item => `${item.nome} - ${item.inicio} - ${item.fim}`)
-        .join("\n");    // transforma a lista em texto
-
-    const blob = new Blob([conteudo], { type: "text/plain" }); // criar "arquivo" em memória
-    const link = document.createElement("a"); // criar link temporário
-
-    /**
-     * .map() Transforma cada item da lista em linha de texto
-     * .join("\n") Junta tudo com quebra de linha
-     *  esse Blob ele cria um "arquivo virtual"
-     */
-
-    link.href = URL.createObjectURL(blob);  // aqui gerar URL do arquivo
-
-    link.download = "resultado.txt"; // defini o nome do arquivo
-
-    link.click(); // simular clique (baixar) -> ele força o download do arquivo
-
-    URL.revokeObjectURL(link.href); // limpar memória
-}
-
-
 
 function carregarEstado() { // aqui reverte o que fizemos e caso um infeliz de f5 as infomações voltam ao normal
     const estadoSalvo = localStorage.getItem("estadoJogo");
@@ -125,21 +87,19 @@ function carregarEstado() { // aqui reverte o que fizemos e caso um infeliz de f
         saldoAtual = dados.saldoAtual;
         contaRodada = dados.contaRodada;
         name = dados.name;
-        lista = dados.lista || [];
     }
 }
 
 export default {
-    saldoJogador, 
+    saldoJogador,
+    defineNome, 
     rodada, 
     atualizacaoSaldo, 
     reset, 
-    txt, 
-    gerarTXT, 
-    salvarEstado, 
-    carregarEstado, 
     getSaldoAtual,
     getSaldoInicio,
     getRodadas,
-    getNome
+    getNome,
+    carregarEstado,
+    salvarEstado
 };
