93d12da6dbfa8633ec2a823b0d257f9ec31c17c8
D4Caa <denilsonboy2000@gmail.com>
2026-05-07 21:00:44 -0300

    Add files via upload
    agora sim tem as coisas que faltava antes, só falta a imagem mudar de acordo com o valor que o código do guilherme entregar, mas eu não to querendo mexer no código do guilherme para não estragar alguma coisa

--- js/effects.js
+++ js/effects.js
@@ -5,33 +5,107 @@ reels.forEach((track, i) => {
 
     const imagensOriginais = track.querySelectorAll(".imagem_reel");
 
-    const altura = 150;
+    const altura = imagensOriginais[0].offsetHeight;
+
     let posicao = 0;
+    let velocidade = 0;
+
+    let animando = false;
+    let desacelerando = false;
+
+    let alvo = 0;
 
     imagensOriginais.forEach(img => {
         track.appendChild(img.cloneNode(true));
     });
 
-    const totalImgs = track.querySelectorAll(".imagem_reel").length;
+    const totalImgs = imagensOriginais.length;
 
+    const limite = altura * totalImgs;
 
-    
+    const indiceFinal = 2;
 
     function animar_giro() {
 
-        posicao += 1 + i;
+        if (!animando) return;
 
-        track.style.transform = `translateY(-${posicao}px)`;
+        if (!desacelerando) {
 
-        if (posicao >= altura * (totalImgs / 2)) {
-            posicao = 0;
+            posicao += velocidade;
+
+            if (posicao >= limite) {
+                posicao -= limite;
+            }
         }
+
+        else {
+
+            let distancia = alvo - posicao;
+
+            if (distancia < 0) {
+                distancia += limite;
+            }
+
+            velocidade = distancia * 0.08;
+
+            if (velocidade > 40) {
+                velocidade = 40;
+            }
+
+            if (velocidade < 0.5) {
+                velocidade = 0.5;
+            }
+
+            posicao += velocidade;
+
+            if (posicao >= limite) {
+                posicao -= limite;
+            }
+
+            if (distancia <= 1) {
+
+                posicao = alvo;
+
+                animando = false;
+                desacelerando = false;
+
+                if (i === reels.length - 1) {
+                    btnGirar.disabled = false;
+                }
+            }
+        }
+
+        track.style.transform = `translate3d(0,-${Math.round(posicao)}px,0)`;
 
         requestAnimationFrame(animar_giro);
     }
-    if (btnGirar) {
-    btnGirar.addEventListener("click", () => {
-    animar_giro();
-    })}
-});
 
+    function paradaSuave() {
+
+        desacelerando = true;
+
+        alvo = indiceFinal * altura + 75;
+    }
+
+    btnGirar.addEventListener("click", () => {
+
+        if (animando) return;
+
+        btnGirar.disabled = true;
+
+        animando = true;
+        desacelerando = false;
+        
+        velocidade = 30 + (i * 5);
+
+        animar_giro();
+
+        setTimeout(() => {
+
+            paradaSuave();
+
+        }, 10000 + (i * 3000));
+
+    });
+
+});
