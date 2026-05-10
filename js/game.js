cdba80600dea396da6c0313540c3eaf9d8ea58fb
GuilhermeO-Gomes <guilherme16062009@gmail.com>
2026-05-10 11:17:33 -0300

    Testes

--- js/game.js
+++ js/game.js
@@ -13,33 +13,40 @@ const imagens = [
   "./assets/cerejinha.webp",
   "./assets/Icone_da_casa.png",
   "./assets/LemonFoot.webp",
-  "./assets/Uvas.webp"
+  "./assets/Uvas.webp",
 ];
+const imagens_comuns = ["./assets/cerejinha.webp", "./assets/Uvas.webp"];
+const imagens_medias = ["./assets/Estrela.png", "./assets/LemonFoot.webp"];
 
 function girar() {
   const tipo = roletar_tipo();
-
+  console.log(tipo);
   let resultadoImgs = [];
 
   switch (tipo) {
     case "Perda total":
       resultadoImgs = [imagens[0], imagens[1], imagens[2]];
+      console.log(resultadoImgs);
       return { ganhou: false, multiplicador: 1, imagens: resultadoImgs };
 
     case "Quase-vitória":
       resultadoImgs = [imagens[1], imagens[1], imagens[2]];
+      console.log(resultadoImgs);
       return { ganhou: false, multiplicador: 1, imagens: resultadoImgs };
 
     case "Vitória pequena":
       resultadoImgs = [imagens[0], imagens[0], imagens[0]];
+      console.log(resultadoImgs);
       return { ganhou: true, multiplicador: 2, imagens: resultadoImgs };
 
     case "Vitória Média":
       resultadoImgs = [imagens[3], imagens[3], imagens[3]];
+      console.log(resultadoImgs);
       return { ganhou: true, multiplicador: 3, imagens: resultadoImgs };
 
     case "JACKPOT":
       resultadoImgs = [imagens[2], imagens[2], imagens[2]];
+      console.log(resultadoImgs);
       return { ganhou: true, multiplicador: 5, imagens: resultadoImgs };
   }
 }
